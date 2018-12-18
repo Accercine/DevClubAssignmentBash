684ddae14c4aea4dfbea9816081c1ea2608f8a4a
Accercine <45529543+Accercine@users.noreply.github.com>
2018-12-18 17:29:42 +0530

    Do reursively to/and take care of subdirectories (makes subdir if they dint exist)

--- backup.sh
+++ backup.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
+backup() {
+  ls $1 | while read p; do
+    if [[ -f $1/$p && ! -f $2/$p ]]
+    then
+      echo $3$p
+      cp $1/$p $2/
+    fi
+    if [ -d $1/$p ]
+    then
+      if [ ! -d $2/$p ]; then
+        mkdir $2/$p
+      fi
+      q=$3
+      backup $1/$p $2/$p $q$p/
+    fi
+  done
+}
 echo Files copied from $1 to $2 are:
-ls $1 | while read p; do
-  if [ ! -e $2/$p ]
-  then
-    echo $p
-    cp $1/$p $2/
-  fi
-done
+backup $1 $2
 echo Files copied from $2 to $1 are:
-ls $2 | while read q; do
-  if [ ! -e $1/$q ]
-  then
-    echo $q
-    cp $2/$q $1/
-  fi
-done
+backup $2 $1
